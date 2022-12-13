685a17c0c5a1ffd3c07519255fb2ea3fcd33267f
Julien Adam <julien.adam@arcodia.fr>
2022-12-13 10:28:20 +0100

    Identity tables set up

--- AspNetBiodiv.Core.Web/Migrations/20221213092726_IdentityScaffolding.Designer.cs
+++ AspNetBiodiv.Core.Web/Migrations/20221213092726_IdentityScaffolding.Designer.cs
@@ -4,6 +4,7 @@ using AspNetBiodiv.Core.Web.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AspNetBiodiv.Core.Web.Migrations
 {
     [DbContext(typeof(EspecesContext))]
-    partial class EspecesContextModelSnapshot : ModelSnapshot
+    [Migration("20221213092726_IdentityScaffolding")]
+    partial class IdentityScaffolding
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
